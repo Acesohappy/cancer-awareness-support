7fd7d1035e48a06f97d17edcabaa23226bd39fbb
Anubama <[anubama2006@gmail.com]>
2025-11-11 21:03:46 +0530

    Skeloton Code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <title>Cancer Awareness</title>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src='./assests/' alt='Banner' className='Banner-image'></img>
+        <h1>Welcome to the Cancer Awareness and Support Page</h1>
       </header>
+      <main>
+        <p>Thankyou for being part of Cancer Awareness program</p>
+
+        <form className='contact-form'>
+          <div className='name'>
+            <label className='label'>Name</label>
+            <input type='text' placeholder='Enter your name' className='input'></input>
+          </div>
+          <div className='Email'>
+            <label className='label'>Email</label>
+            <input type='email' placeholder='Enter your email id' className='input'></input>
+          </div>
+          <div className='Message'>
+            <label className='label'>Your Message</label>
+            <textarea placeholder='your message' className='input'/>
+          </div>
+        </form>
+      </main>
+      <footer>
+        <p>@All rights are reserved</p>
+      </footer>
     </div>
   );
 }
